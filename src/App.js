d62cb3ca9a86f4a416ac4139e671b7475998e70a
Robbie Gay <robbiecgay@gmail.com>
2019-12-30 14:23:28 -0500

    Worked through remaining MC sections 8-12

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import React from 'react';
-import './App.css';
+import Clock from './Clock';
 
-class Clock extends React.Component {
+class Controller extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = {
+      format: "tic",
+    }
   }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
   render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        {console.log(this.state.date.getSeconds() % 2 === 0)}
-        {this.state.date.getSeconds() % 2 === 0 ?
-          <h2>TIC</h2> :
-          <h2>TOC</h2>
-        }
-      </div>
+    return(
+      <Clock type={this.state.format} />
     );
   }
 }
 
 function App() {
   return (
-    <Clock />
+    <Controller />
   );
 }
 
